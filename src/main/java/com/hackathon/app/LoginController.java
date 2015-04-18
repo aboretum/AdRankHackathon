274c0a4e02949024b346d22973e003f571cff8c4
wuchen <wwfyc798@gmail.com>
2015-04-18 08:03:31 -0500

    Added page for sign-up

---
+++ src/main/java/com/hackathon/app/LoginController.java
@@ -0,0 +1,39 @@
+package com.hackathon.app;
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Handles requests for the user login from sign-in page.
+ */
+@Controller
+public class LoginController {
+	
+	private static final Logger logger = LoggerFactory.getLogger(LoginController.class);
+	
+	/**
+	 * Processing the user input for credentials to authenticate the user.
+	 */
+	@RequestMapping(value = "/Login", method = RequestMethod.GET)
+	public String login(Locale locale, Model model) {
+		logger.info("Welcome home! The client locale is {}.", locale);
+		
+		Date date = new Date();
+		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+		
+		String formattedDate = dateFormat.format(date);
+		
+		model.addAttribute("serverTime", formattedDate );
+		
+		return "index";
+	}
+	
+}
