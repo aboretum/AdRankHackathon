05c543c8f60af195686229fe82c1892867492a04
rc029502 <rishab.chaturvedi@cerner.com>
2015-04-19 13:45:02 -0500

    Added Search page (search.html) and getting results on search. Successful login should redirect user to search.html.

--- src/main/webapp/resources/scripts/main.js
+++ src/main/webapp/resources/scripts/main.js
@@ -3,14 +3,16 @@
 //called when the user clicks the Login button on the login screen
 function loginUser(){
 	// ajax call 
-	alert("loginUser working");	
+	//alert("loginUser working");	
+
+	//on login navigate to search.html , window.location.href to search.html
 
 }
 
 //called when the user clicks the Submit button on the Forgot Password screen
 function forgotPassword(){
 	// ajax call
-	alert("forgotPassword working");
+	//alert("forgotPassword working");
 
 }
 
@@ -21,22 +23,68 @@ function signupUser(){
 	var email = $('#email').val();
 	var firstName = $('#fname').val();
 	var lastName = $('#lname').val();
-	alert("works");
+	//alert("works");
 	var password = $('#pwd').val();
 	var gender = $( '#gender').val();
 
-	alert(email);
-	alert(firstName);
-	alert(lastName);
-	alert(password);
-	alert(gender);
 	// ajax call
-	alert("signupUser working");
 
 }
 
 
-function searchVideos(){
 
-	alert("searchVideos working");
+// function loadResults(){
+// 	alert("test");
+// 	alert("working");
+// 	alert($('#searchText').val());
+// 	// get the result object here and use the asme loop to form an object same as ajax call 
+
+
+// //	$.ajax({
+// //     url: 'http://gdata.youtube.com/feeds/mobile/videos?alt=json-in-script&q=' + query,
+// //     dataType: 'jsonp',
+// //     success: function (data) {
+// //     	alert("success");
+// //         var row = "";
+// //         for (i = 0; i < data.feed.entry.length; i++) {
+// //             row += "<div class='search_item'>";
+// //             row += "<table width='100%'>";
+// //             row += "<tr>";
+// //             row += "<td vAlign='top' align='left'>";
+// //             row += "<a href='#' ><img width='120px' height='80px' src=" + data.feed.entry[i].media$group.media$thumbnail[0].url + " /></a>";
+// //             row += "</td>";
+// //             row += "<td vAlign='top' width='100%' align='left'>";
+// //             row += "<a href='#' ><b>" + data.feed.entry[i].media$group.media$title.$t + "</b></a><br/>";
+// //             row += "<span style='font-size:12px; color:#555555'>by " + data.feed.entry[i].author[0].name.$t + "</span><br/>";
+// //             row += "<span style='font-size:12px' color:#666666>" + data.feed.entry[i].yt$statistics.viewCount + " views" + "<span><br/>";
+// //             row += "</td>";
+// //             row += "</tr>";
+// //             row += "</table>";
+// //             row += "</div>";
+// //         }
+// //         document.getElementById("search-results-block").innerHTML = row;
+// //     },
+// //     error: function () {
+// //         alert("Error loading youtube video results");
+// //     }
+// // });
+// return false;
+// }
+
+
+
+// loop through the user list to populate
+function populateFrequentUsers(){
+
+	var ul = document.getElementById("frequentUsers");
+
+	// insert for loop here
+	
+	var li = document.createElement("li");
+	li.className = "list-group-item";
+	//li.addClass("list-group-item");
+	li.appendChild(document.createTextNode("Four"));
+	ul.appendChild(li);
+
+	// for loop ends
 }
